a0a1c8d8c8b2ab5c50a04bfc264de0dcd8f458ea
kimnoeun <1203shdms@naver.com>
2021-05-06 15:50:17 +0900

    오디오 스트리밍

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, Response,render_template
+import pyaudio
+import speech_recognition as sr
+
+app = Flask(__name__)
+
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 5
+
+
+audio1 = pyaudio.PyAudio()
+
+def genHeader(sampleRate, bitsPerSample, channels):
+    datasize = 2000*10**6
+    o = bytes("RIFF",'ascii')                                               # (4byte) Marks file as RIFF
+    o += (datasize + 36).to_bytes(4,'little')                               # (4byte) File size in bytes excluding this and RIFF marker
+    o += bytes("WAVE",'ascii')                                              # (4byte) File type
+    o += bytes("fmt ",'ascii')                                              # (4byte) Format Chunk Marker
+    o += (16).to_bytes(4,'little')                                          # (4byte) Length of above format data
+    o += (1).to_bytes(2,'little')                                           # (2byte) Format type (1 - PCM)
+    o += (channels).to_bytes(2,'little')                                    # (2byte)
+    o += (sampleRate).to_bytes(4,'little')                                  # (4byte)
+    o += (sampleRate * channels * bitsPerSample // 8).to_bytes(4,'little')  # (4byte)
+    o += (channels * bitsPerSample // 8).to_bytes(2,'little')               # (2byte)
+    o += (bitsPerSample).to_bytes(2,'little')                               # (2byte)
+    o += bytes("data",'ascii')                                              # (4byte) Data Chunk Marker
+    o += (datasize).to_bytes(4,'little')                                    # (4byte) Data size in bytes
+    return o
+
+
+#페이지 새로고침때마다 말끝날때까지 음성 입력 받아서 웹 화면에 띄우기 
+@app.route('/')
+def index():
+    transcript=""
+    recognizer =  sr.Recognizer()        
+    with sr.Microphone() as source:
+        print('Speak Anything : ')
+        audio = recognizer.listen(source)
+    try:
+        transcript = recognizer.recognize_google(audio,language='ko-KR')
+        print('You said : {}'.format(transcript))
+    except:
+        print('Sorry could not recognize yur voice')           
+           
+    return render_template('index.html', transcript=transcript)
+
+
+#이건 오류남...오디오 스트리밍 서버였는디
+@app.route('/audio')
+def audio():
+    # start Recording
+    def sound():
+
+        CHUNK = 1024
+        sampleRate = 44100
+        bitsPerSample = 16
+        channels = 2
+        wav_header = genHeader(sampleRate, bitsPerSample, channels)
+
+        stream = audio1.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,input_device_index=1,
+                        frames_per_buffer=CHUNK)
+        print("recording...")
+        #frames = []
+        first_run = True
+        while True:
+           if first_run:
+               data = wav_header + stream.read(CHUNK)
+               first_run = False
+           else:
+               data = stream.read(CHUNK)
+           yield(data)
+
+    return Response(sound())
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
