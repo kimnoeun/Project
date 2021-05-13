8658a3688266e1dce79f0fe570536b1bd52622ce
kimnoeun <1203shdms@naver.com>
2021-05-13 22:34:13 +0900

    오디오 스트리밍 + 웹캠 화면 스트리밍

--- test.py
+++ test.py
@@ -9,33 +9,6 @@ import sys
 import os
 
 
-from gaze_tracking import GazeTracking
-
-gaze = GazeTracking()
-webcam = cv2.VideoCapture(0)
- 
-while True:
-    _, frame = webcam.read()
-    gaze.refresh(frame)
-
-    new_frame = gaze.annotated_frame()
-    text = ""
-
-    if gaze.is_right():
-        text = "Looking right"
-    elif gaze.is_left():
-        text = "Looking left"
-    elif gaze.is_center():
-        text = "Looking center"
-
-    cv2.putText(new_frame, text, (60, 60), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
-    cv2.imshow("Demo", new_frame)
-
-    if cv2.waitKey(1) == 27:
-        break
-
-
-
 
 
 
@@ -109,6 +82,8 @@ def genHeader(sampleRate, bitsPerSample, channels):
     return o
 
 
+
+
 #뭐라 말했는지 음성인식
 @app.route('/upload/')
 def uploaduj():
@@ -129,6 +104,7 @@ def uploaduj():
         cheat=1
 
     return render_template('index2.html', transcript=transcript, cheat=cheat)
+
 
 
 #오디오 스트리밍 서버였는디
