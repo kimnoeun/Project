b09c9c3f40fad808d83537cec2ba958a8698fd9f
kimnoeun <1203shdms@naver.com>
2021-06-06 19:07:11 +0900

    haar 알고리즘과 roi동공추출로 정확한 시선 추적을 해보려 했으나 실패

--- 2021/6/20210606.py
+++ 2021/6/20210606.py
@@ -1,22 +1,40 @@
 import cv2
-import numpy as np
+ 
+font = cv2.FONT_ITALIC
+ 
 
-cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "./haarcascade_frontalface_default.xml")  # 얼굴찾기 haar 파일
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "./haarcascade_eye.xml") # 눈찾기 haar 파일
+ 
+cam = cv2.VideoCapture(0)
 
+ 
 while True:
-    ret, frame = cap.read()
+    ret, frame = cam.read()
+    if ret is False:
+        break
     
-    roi = frame[20: 300, 27: 396]
-    
+    roi = frame[0:500,0:500]
     rows, cols, _ = roi.shape
-
     gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    gray_roi = cv2.GaussianBlur(gray_roi, (7, 7), 0)
 
-    _, threshold = cv2.threshold(gray_roi, 22, 255, cv2.THRESH_BINARY_INV)
+
+    faces = face_cascade.detectMultiScale(gray_roi,1.3, 5)
+
+    for(x,y, w,h) in faces:
+        roi_gray = gray_roi[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0,255,0), 2)
+ 
+ 
+
+    #gray_roi = cv2.GaussianBlur(gray_roi, (7, 7), 0)
+    _, threshold = cv2.threshold(gray_roi, 25, 255, cv2.THRESH_BINARY_INV)
     contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
-    
+
     for cnt in contours:
         (x, y, w, h) = cv2.boundingRect(cnt)
         cv2.drawContours(roi, [cnt], -1, (0, 0, 255), 3)
@@ -28,8 +46,8 @@ while True:
     cv2.imshow("Threshold", threshold)
     cv2.imshow("gray roi", gray_roi)
     cv2.imshow("Roi", roi)
-
     key = cv2.waitKey(30)
     if key == 27:
         break
 cv2.destroyAllWindows()
+ 
